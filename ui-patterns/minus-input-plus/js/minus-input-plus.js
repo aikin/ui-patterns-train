61ee95e538d1efb07867c0bafe3d67d13de785e9
aikin <me@ulaijn.com>
2016-02-03 23:50:55 +0800

    aikin: refactor minus input plus

--- ui-patterns/minus-input-plus/js/minus-input-plus.js
+++ ui-patterns/minus-input-plus/js/minus-input-plus.js
@@ -1,16 +1,12 @@
 $(function () {
 
 	$('.minus-input-plus__minus').on('click', function () {
-		$(this)
-			.parent()
-			.siblings('input')
-			.val(parseInt($(this).parent().siblings('input').val()) - 1)
+		var $inputSibling = $(this).parent().siblings('input');
+        $inputSibling.val(parseInt($inputSibling.val()) - 1);
 	});
 
 	$('.minus-input-plus__plus').on('click', function () {
-		$(this)
-			.parent()
-			.siblings('input')
-			.val(parseInt($(this).parent().siblings('input').val()) + 1)
+		var $inputSibling = $(this).parent().siblings('input');
+		$inputSibling.val(parseInt($inputSibling.val()) + 1);
 	})
 });
